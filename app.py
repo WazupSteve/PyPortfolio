39662e0d06ad440e3382f862f76a0ca6caf83628
Lalith <lalithae30@gmail.com>
2024-04-13 13:49:06 +0530

    Fixed the error

--- app.py
+++ app.py
@@ -12,120 +12,142 @@ from main1 import *
 import ast
 
 # --- Gradio Interface ---
+
+
 def gradio_interface(
-    tickers: str = "MSFT, AMZN, KO, MA, COST, LUV, XOM, PFE, JPM, UNH, ACN, DIS, GILD, F, TSLA",
-    period: str = "max",
-    frequency: int = 252,
-    weight_bounds_str: str = "None, None",
-    sector_mapper_str: str = '{"MSFT": "Tech", "AMZN": "Tech", "KO": "Consumer Staples", "MA": "Financial Services", "COST": "Consumer Staples", "LUV": "Aerospace", "XOM": "Energy", "PFE": "Healthcare", "JPM": "Financial Services", "UNH": "Healthcare", "ACN": "Tech", "DIS": "Media", "GILD": "Healthcare", "F": "Auto", "TSLA": "Auto"}',
-    sector_lower_str: str = '{"Consumer Staples": 0.1, "Tech": 0.05}',
-    sector_upper_str: str = '{"Tech": 0.2, "Aerospace": 0.1, "Energy": 0.1, "Auto": 0.15}',
-    objective_function: str = "max_sharpe",
-    gamma: float = 0.0,
-    target_volatility: float = 0.0,
-    target_return: float = 0.0,
-    market_neutral: bool = False,
-    total_portfolio_value: int = 20000,
-    short_ratio: float = 0.3,
-    target_cvar: float = -0.025,
-    max_big_tech_weight: float = 0.3,
+    tickers: str,
+    period: str,
+    frequency: int,
+    weight_bounds_str: str,
+    sector_mapper_str: str,
+    sector_lower_str: str,
+    sector_upper_str: str,
+    objective_function: str,
+    gamma: float,
+    target_volatility: float,
+    target_return: float,
+    market_neutral: bool,
+    total_portfolio_value: int,
+    short_ratio: float,
+    target_cvar: float,
+    max_big_tech_weight: float,
 ):
     # --- Input Validation and Processing ---
-    try:
-        tickers = [t.strip() for t in tickers.upper().split(",")]  # Remove spaces
-        weight_bounds = tuple(float(w) if w.strip().lower() != "none" else None for w in weight_bounds_str.split(","))
-        sector_mapper = ast.literal_eval(sector_mapper_str)
-        sector_lower = ast.literal_eval(sector_lower_str)
-        sector_upper = ast.literal_eval(sector_upper_str)
+    tickers = [t.strip()
+               for t in tickers.upper().split(",")]  # Remove spaces
+    weight_bounds = tuple(float(w) if w.strip().lower(
+    ) != "none" else None for w in weight_bounds_str.split(","))
+    sector_mapper = ast.literal_eval(sector_mapper_str)
+    sector_lower = ast.literal_eval(sector_lower_str)
+    sector_upper = ast.literal_eval(sector_upper_str)
 
-        # Fetch historical prices
-        prices = fetch_historical_prices(tickers, period=period)
+    # Fetch historical prices
+    prices = fetch_historical_prices(tickers, period=period)
 
-        # Calculate expected returns and covariance matrix
-        mu = calculate_expected_returns(prices)
-        cov_matrix = calculate_sample_covariance(prices, frequency=frequency)
+    # Calculate expected returns and covariance matrix
+    mu = calculate_expected_returns(prices)
+    cov_matrix = calculate_sample_covariance(prices, frequency=frequency)
 
-        # Portfolio optimization
-        weights, performance = optimize_portfolio(prices, mu, cov_matrix, 
-            weight_bounds=weight_bounds,
-            sector_mapper=sector_mapper,
-            sector_lower=sector_lower,
-            sector_upper=sector_upper,
-            objective_function=objective_function,
-            gamma=gamma,
-            target_volatility=target_volatility,
-            target_return=target_return,
-            market_neutral=market_neutral
-        )
+    # Portfolio optimization
+    weights, performance = optimize_portfolio(prices, mu, cov_matrix,
+                                              weight_bounds=weight_bounds,
+                                              sector_mapper=sector_mapper,
+                                              sector_lower=sector_lower,
+                                              sector_upper=sector_upper,
+                                              objective_function=objective_function,
+                                              gamma=gamma,
+                                              target_volatility=target_volatility,
+                                              target_return=target_return,
+                                              market_neutral=market_neutral
+                                              )
 
-        # Discrete allocation
-        latest_prices = prices.iloc[-1]
-        alloc = allocate_portfolio(weights, latest_prices, total_portfolio_value=total_portfolio_value, short_ratio=short_ratio)
+    # Discrete allocation
+    latest_prices = prices.iloc[-1]
+    alloc = allocate_portfolio(
+        weights, latest_prices, total_portfolio_value=total_portfolio_value, short_ratio=short_ratio)
 
-        # Sector allocation
-        sector_allocation = map_sectors_to_tickers(tickers, sector_mapper, weights)
+    # Sector allocation
+    sector_allocation = str(map_sectors_to_tickers(
+        tickers, sector_mapper, weights))
 
-        # For compute_cvar
-        if prices is None or mu is None:
-            cvar_performance = ("Unable to compute CVaR: prices or expected returns are missing", None)
-        else:
-            returns = expected_returns.returns_from_prices(prices).dropna()
-            cvar_performance = compute_cvar(prices, mu, returns, target_cvar=target_cvar)
+    # For compute_cvar
+    if prices is None or mu is None:
+        cvar_performance = (
+            "Unable to compute CVaR: prices or expected returns are missing", None)
+    else:
+        returns = expected_returns.returns_from_prices(prices).dropna()
+        cvar_performance = compute_cvar(
+            prices, mu, returns, target_cvar=target_cvar)
 
-        # For perform_cla_optimization
-        if mu is None or cov_matrix is None:
-            cla_performance = ("Unable to perform CLA optimization: expected returns or covariance matrix is missing", None)
-        else:
-            cla_performance = perform_cla_optimization(mu, cov_matrix)
+    # For perform_cla_optimization
+    if mu is None or cov_matrix is None:
+        cla_performance = (
+            "Unable to perform CLA optimization: expected returns or covariance matrix is missing", None)
+    else:
+        cla_performance = perform_cla_optimization(mu, cov_matrix)
 
-        # Constraint optimization
-        big_tech_indices = [tickers.index(ticker) for ticker in {"MSFT", "AMZN", "TSLA"}]
-        if not tickers or not big_tech_indices:
-            constraint_performance = ("Unable to perform constraint optimization: tickers or big tech indices are missing", None)
-        else:
-            constraint_performance = perform_constraint_optimization(mu, cov_matrix, tickers, big_tech_indices, max_big_tech_weight=max_big_tech_weight)
+    # Constraint optimization
+    big_tech_indices = [tickers.index(ticker)
+                        for ticker in {"MSFT", "AMZN", "TSLA"}]
+    if not tickers or not big_tech_indices:
+        constraint_performance = (
+            "Unable to perform constraint optimization: tickers or big tech indices are missing", None)
+    else:
+        constraint_performance = perform_constraint_optimization(
+            mu, cov_matrix, tickers, big_tech_indices, max_big_tech_weight=max_big_tech_weight)
 
-        # Efficient Frontier plot
-        if constraint_performance is None:
-            efficient_frontier_plot = None
-        else:
-            fig, ax = plt.subplots()
-            plot_efficient_frontier_with_random_portfolios(constraint_performance, ax=ax)
-            plt.close(fig)  
-            efficient_frontier_plot = fig.get_children()[0]  # Access the plot
+    # Efficient Frontier plot
+    if constraint_performance is None:
+        efficient_frontier_plot = None
+    else:
+        fig, ax = plt.subplots()
+        plot_efficient_frontier_with_random_portfolios(
+            constraint_performance, ax=ax)
+        efficient_frontier_plot = fig  # Access the plot
 
-        # Formatting output
-        performance_text = performance[0]
-        cvar_performance_text = cvar_performance[0]
-        cla_performance_text = cla_performance[0]
-        constraint_performance_text = constraint_performance[0]
-
-    except Exception as e:
-        return f"Error processing inputs: {e}", None, None, None, None, None, None
-
-    return [value if value is not None else "Error" for value in [performance_text, alloc.to_frame(), str(sector_allocation), cvar_performance_text, cla_performance_text, constraint_performance_text, efficient_frontier_plot]]
+    # Formatting output
+    performance_text = str(performance[0])
+    cvar_performance_text = str(cvar_performance[0])
+    cla_performance_text = str(cla_performance[0])
+    constraint_performance_text = str(
+        constraint_performance.portfolio_performance(verbose=True)[0])
+    return performance_text, alloc.to_dict(), sector_allocation, cvar_performance_text, cla_performance_text, constraint_performance_text, efficient_frontier_plot
+    return tuple(value for value in [performance_text, alloc.to_frame(), sector_allocation, cvar_performance_text, cla_performance_text, constraint_performance_text, efficient_frontier_plot])
 
 
 # --- Gradio Interface Definition ---
 iface = gr.Interface(
     fn=gradio_interface,
     inputs=[
-        gr.Textbox(label="Tickers (comma-separated)", value="MSFT, AMZN, KO, MA, COST, LUV, XOM, PFE, JPM, UNH, ACN, DIS, GILD, F, TSLA"),
-        gr.Dropdown(label="Period", choices=["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"], value="max"),
-        gr.Slider(minimum=1, maximum=365, value=252, step=1, label="Frequency"),
+        gr.Textbox(label="Tickers (comma-separated)",
+                   value="MSFT, AMZN, KO, MA, COST, LUV, XOM, PFE, JPM, UNH, ACN, DIS, GILD, F, TSLA"),
+        gr.Dropdown(label="Period", choices=[
+                    "1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"], value="max"),
+        gr.Slider(minimum=1, maximum=365, value=252,
+                  step=1, label="Frequency"),
         gr.Textbox(label="Weight Bounds (comma-separated)", value="0.0, 0.3"),
-        gr.Textbox(label="Sector Mapper (JSON format)", value='{"MSFT": "Tech", "AMZN": "Tech", "KO": "Consumer Staples", "MA": "Financial Services", "COST": "Consumer Staples", "LUV": "Aerospace", "XOM": "Energy", "PFE": "Healthcare", "JPM": "Financial Services", "UNH": "Healthcare", "ACN": "Tech", "DIS": "Media", "GILD": "Healthcare", "F": "Auto", "TSLA": "Auto"}'),
-        gr.Textbox(label="Sector Lower Bounds (JSON format)", value='{"Consumer Staples": 0.1, "Tech": 0.05}'),
-        gr.Textbox(label="Sector Upper Bounds (JSON format)", value='{"Tech": 0.2, "Aerospace": 0.1, "Energy": 0.1, "Auto": 0.15}'),
-        gr.Dropdown(label="Objective Function", choices=["max_sharpe", "min_volatility", "efficient_risk", "efficient_return"], value="max_sharpe"),
-        gr.Slider(minimum=0.0, maximum=1.0, value=0.0, step=0.01, label="Gamma (L2 regularization)"),
-        gr.Slider(minimum=0.0, maximum=1.0, value=0.0, step=0.01, label="Target Volatility"),
-        gr.Slider(minimum=0.0, maximum=1.0, value=0.0, step=0.01, label="Target Return"),
+        gr.Textbox(label="Sector Mapper (JSON format)",
+                   value='{"MSFT": "Tech", "AMZN": "Tech", "KO": "Consumer Staples", "MA": "Financial Services", "COST": "Consumer Staples", "LUV": "Aerospace", "XOM": "Energy", "PFE": "Healthcare", "JPM": "Financial Services", "UNH": "Healthcare", "ACN": "Tech", "DIS": "Media", "GILD": "Healthcare", "F": "Auto", "TSLA": "Auto"}'),
+        gr.Textbox(label="Sector Lower Bounds (JSON format)",
+                   value='{"Consumer Staples": 0.1, "Tech": 0.05}'),
+        gr.Textbox(label="Sector Upper Bounds (JSON format)",
+                   value='{"Tech": 0.2, "Aerospace": 0.1, "Energy": 0.1, "Auto": 0.15}'),
+        gr.Dropdown(label="Objective Function", choices=[
+                    "max_sharpe", "min_volatility", "efficient_risk", "efficient_return"], value="max_sharpe"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.0,
+                  step=0.01, label="Gamma (L2 regularization)"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.0,
+                  step=0.01, label="Target Volatility"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.0,
+                  step=0.01, label="Target Return"),
         gr.Checkbox(label="Market Neutral", value=False),
         gr.Number(label="Total Portfolio Value", value=20000),
-        gr.Slider(minimum=0.0, maximum=1.0, value=0.3, step=0.01, label="Short Ratio"),
-        gr.Slider(minimum=-0.1, maximum=0, value=-0.025, step=0.001, label="Target CVaR"),
-        gr.Slider(minimum=0.0, maximum=1.0, value=0.3, step=0.01, label="Max Big Tech Weight"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.3,
+                  step=0.01, label="Short Ratio"),
+        gr.Slider(minimum=0, maximum=0.1, value=0.025,
+                  step=0.001, label="Target CVaR"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.3,
+                  step=0.01, label="Max Big Tech Weight"),
     ],
     outputs=[
         gr.Textbox(label="Portfolio Performance"),
@@ -139,7 +161,4 @@ iface = gr.Interface(
     title="Portfolio Optimization App",
     description="This Gradio application allows you to optimize a portfolio using various techniques and constraints.",
 )
-iface.launch(share=True)
-
-
-
+iface.launch(share=False)
--- main1.py
+++ main1.py
@@ -7,36 +7,43 @@ from pypfopt.cla import CLA
 import cvxpy as cp
 from pypfopt.discrete_allocation import DiscreteAllocation
 
+
 def fetch_historical_prices(tickers, period="max"):
     """Fetch historical stock prices from Yahoo Finance."""
     ohlc = yf.download(tickers, period=period)
     prices = ohlc["Adj Close"].dropna(how="all")
     return prices
 
+
 def calculate_sample_covariance(prices, frequency=252):
     """Calculate sample covariance matrix."""
     return risk_models.sample_cov(prices, frequency=frequency)
+
 
 def calculate_shrunk_covariance(prices):
     """Calculate shrunk covariance matrix."""
     return risk_models.CovarianceShrinkage(prices).ledoit_wolf()
 
+
 def calculate_expected_returns(prices):
     """Calculate expected returns using CAPM model."""
     return expected_returns.capm_return(prices)
+
 
 def plot_covariance_matrix(cov_matrix, plot_correlation=True):
     """Plot covariance matrix."""
     plotting.plot_covariance(cov_matrix, plot_correlation=plot_correlation)
 
+
 def plot_histogram(data, bins=50):
     """Plot histogram."""
     data.hist(bins=bins)
 
+
 def optimize_portfolio(prices, mu, cov_matrix, weight_bounds=(None, None), sector_mapper=None, sector_lower=None, sector_upper=None, objective_function=None, gamma=None, target_volatility=None, target_return=None, market_neutral=False):
     """Optimize portfolio."""
     ef = EfficientFrontier(mu, cov_matrix, weight_bounds=weight_bounds)
-    
+
     if sector_mapper:
         ef.add_sector_constraints(sector_mapper, sector_lower, sector_upper)
 
@@ -47,7 +54,8 @@ def optimize_portfolio(prices, mu, cov_matrix, weight_bounds=(None, None), secto
     elif objective_function == "efficient_risk":
         ef.efficient_risk(target_volatility=target_volatility)
     elif objective_function == "efficient_return":
-        ef.efficient_return(target_return=target_return, market_neutral=market_neutral)
+        ef.efficient_return(target_return=target_return,
+                            market_neutral=market_neutral)
 
     if gamma:
         ef.add_objective(objective_functions.L2_reg, gamma=gamma)
@@ -57,27 +65,28 @@ def optimize_portfolio(prices, mu, cov_matrix, weight_bounds=(None, None), secto
     return weights, performance
 
 
-import pandas as pd
-
 def allocate_portfolio(weights, latest_prices, total_portfolio_value, short_ratio=0.3):
     # Get the intersection of tickers between weights and latest_prices
-    common_tickers = list(set(weights.keys()) & set(latest_prices.dropna().index))
-    
+    common_tickers = list(set(weights.keys()) &
+                          set(latest_prices.dropna().index))
+
     # Filter the weights and latest_prices to include only common tickers
     filtered_weights = {ticker: weights[ticker] for ticker in common_tickers}
     filtered_prices = latest_prices[common_tickers]
-    
-    da = DiscreteAllocation(filtered_weights, filtered_prices, total_portfolio_value=total_portfolio_value, short_ratio=short_ratio)
+
+    da = DiscreteAllocation(filtered_weights, filtered_prices,
+                            total_portfolio_value=total_portfolio_value, short_ratio=short_ratio)
     alloc, leftover = da.lp_portfolio()
     print(f"Discrete allocation performed with ${leftover:.2f} leftover")
-    
+
     # Convert alloc to a Pandas Series
     alloc_series = pd.Series(alloc)
-    
+
     # Add missing tickers with zero allocation
     alloc_series = alloc_series.reindex(weights.keys(), fill_value=0)
-    
+
     return alloc_series
+
 
 def map_sectors_to_tickers(tickers, sector_mapper, weights):
     """Map tickers to sectors."""
@@ -89,16 +98,19 @@ def map_sectors_to_tickers(tickers, sector_mapper, weights):
         sector_allocation[sector] += weights[ticker]
     return sector_allocation
 
-def compute_cvar(prices, mu, returns, target_cvar=-0.025):
+
+def compute_cvar(prices, mu, returns, target_cvar=0.025):
     """Compute Conditional Value at Risk (CVaR)."""
-    if target_cvar < 0:
-        target_cvar = 0.01  # Set a non-negative default value
-        print(f"Warning: target_cvar was negative, setting it to {target_cvar}")
+    # if target_cvar < 0:
+    #     target_cvar = 0.01  # Set a non-negative default value
+    #     print(
+    #         f"Warning: target_cvar was negative, setting it to {target_cvar}")
 
     semicov = risk_models.semicovariance(prices, benchmark=0)
     ec = EfficientCVaR(mu, semicov)
     ec.efficient_risk(target_cvar=target_cvar)
     return ec.portfolio_performance(verbose=True)
+
 
 def perform_cvar_optimization(prices, mu, returns, target_cvar=-0.025):
     """Perform CVaR optimization."""
@@ -106,11 +118,13 @@ def perform_cvar_optimization(prices, mu, returns, target_cvar=-0.025):
     ec.efficient_risk(target_cvar=target_cvar)
     return ec.portfolio_performance(verbose=True)
 
+
 def perform_cla_optimization(mu, cov_matrix):
     """Perform Critical Line Algorithm (CLA) optimization."""
     cla = CLA(mu, cov_matrix)
     cla.max_sharpe()
-    return cla.portfolio_performance(verbose=True) 
+    return cla.portfolio_performance(verbose=True)
+
 
 def perform_constraint_optimization(mu, cov_matrix, tickers, big_tech_indices, max_big_tech_weight=0.3):
     """Perform portfolio optimization with constraints."""
@@ -122,12 +136,14 @@ def perform_constraint_optimization(mu, cov_matrix, tickers, big_tech_indices, m
     ef = EfficientFrontier(mu, cov_matrix)
 
     # Create a lambda function for the constraint
-    big_tech_constraint = lambda w: cp.sum([w[idx] for idx in big_tech_indices]) <= max_big_tech_weight
+    def big_tech_constraint(w): return cp.sum(
+        [w[idx] for idx in big_tech_indices]) <= max_big_tech_weight
 
     ef.add_constraint(big_tech_constraint)
     ef.max_sharpe()  # Optimize the portfolio with the added constraint
     ef.clean_weights()  # Update the weights with the added constraint
     return ef
+
 
 def _ef_default_returns_range(ef, n_samples=10000):
     """Generate a range of expected returns for the efficient frontier."""
@@ -136,6 +152,7 @@ def _ef_default_returns_range(ef, n_samples=10000):
         w = np.random.dirichlet(np.ones(len(ef.expected_returns)), 1)
         mus.append(w.dot(ef.expected_returns)[0])
     return np.linspace(min(mus), max(mus), n_samples), mus
+
 
 def plot_efficient_frontier_with_random_portfolios(ef, ax=None, n_samples=10000, show_assets=True):
     if ax is None:
@@ -161,6 +178,7 @@ def plot_efficient_frontier_with_random_portfolios(ef, ax=None, n_samples=10000,
     ax.legend()
     return ax
 
+
 def get_efficient_frontier_data(ef, n_samples=10000, show_assets=True):
     # Ensure these are methods and not overwritten
     ef_param_range = _ef_default_returns_range(ef, n_samples)
@@ -181,78 +199,86 @@ def get_efficient_frontier_data(ef, n_samples=10000, show_assets=True):
 
     return ef_data, assets_data
 
+
 def main():
-    tickers = ["MSFT","AMZN","KO","MA","COST","LUV","XOM","PFE","JPM","UNH","ACN","DIS","GILD","F","TSLA"]
-    
+    tickers = ["MSFT", "AMZN", "KO", "MA", "COST", "LUV", "XOM",
+               "PFE", "JPM", "UNH", "ACN", "DIS", "GILD", "F", "TSLA"]
+
     prices = fetch_historical_prices(tickers)
     sample_cov = calculate_sample_covariance(prices)
-    shrunk_cov = calculate_shrunk_covariance(prices) 
+    shrunk_cov = calculate_shrunk_covariance(prices)
     mu = calculate_expected_returns(prices)
 
     # Portfolio optimization (example with max_sharpe)
-    weights, performance = optimize_portfolio(prices, mu, sample_cov, objective_function="max_sharpe")
+    weights, performance = optimize_portfolio(
+        prices, mu, sample_cov, objective_function="max_sharpe")
 
     # Discrete allocation (example)
     latest_prices = prices.iloc[-1]
-    alloc = allocate_portfolio(weights, latest_prices, total_portfolio_value=20000) 
+    alloc = allocate_portfolio(
+        weights, latest_prices, total_portfolio_value=20000)
 
     # Sector mapping (example)
     sector_mapper = {
-    "MSFT": "Tech",
-    "AMZN": "Consumer Discretionary",
-    "KO": "Consumer Staples",
-    "MA": "Financial Services",
-    "COST": "Consumer Staples",
-    "LUV": "Aerospace",
-    "XOM": "Energy",
-    "PFE": "Healthcare",
-    "JPM": "Financial Services",
-    "UNH": "Healthcare",
-    "ACN": "Misc",
-    "DIS": "Media",
-    "GILD": "Healthcare",
-    "F": "Auto",
-    "TSLA": "Auto"
+        "MSFT": "Tech",
+        "AMZN": "Consumer Discretionary",
+        "KO": "Consumer Staples",
+        "MA": "Financial Services",
+        "COST": "Consumer Staples",
+        "LUV": "Aerospace",
+        "XOM": "Energy",
+        "PFE": "Healthcare",
+        "JPM": "Financial Services",
+        "UNH": "Healthcare",
+        "ACN": "Misc",
+        "DIS": "Media",
+        "GILD": "Healthcare",
+        "F": "Auto",
+        "TSLA": "Auto"
     }
     sector_lower = {
-        "Consumer Staples": 0.1, # at least 10% to staples
-        "Tech": 0.05 # at least 5% to tech
+        "Consumer Staples": 0.1,  # at least 10% to staples
+        "Tech": 0.05  # at least 5% to tech
         # For all other sectors, it will be assumed there is no lower bound
     }
 
     sector_upper = {
         "Tech": 0.2,
-        "Aerospace":0.1,
+        "Aerospace": 0.1,
         "Energy": 0.1,
-        "Auto":0.15
+        "Auto": 0.15
     }
-    sector_allocation = map_sectors_to_tickers(tickers, sector_mapper, weights) 
+    sector_allocation = map_sectors_to_tickers(tickers, sector_mapper, weights)
     print("Sector allocation:", sector_allocation)
 
     # CVaR optimization (example)
-    returns = expected_returns.returns_from_prices(prices).dropna() 
-    cvar_performance = compute_cvar(prices, mu, returns) 
+    returns = expected_returns.returns_from_prices(prices).dropna()
+    cvar_performance = compute_cvar(prices, mu, returns)
 
     # CLA optimization (example)
-    cla_performance = perform_cla_optimization(mu, sample_cov) 
+    cla_performance = perform_cla_optimization(mu, sample_cov)
 
     # Constraint optimization (example)
-    big_tech_indices = [tickers.index(ticker) for ticker in {"MSFT", "AMZN", "TSLA"}] 
-    ef = perform_constraint_optimization(mu, sample_cov, tickers, big_tech_indices)
+    big_tech_indices = [tickers.index(ticker)
+                        for ticker in {"MSFT", "AMZN", "TSLA"}]
+    ef = perform_constraint_optimization(
+        mu, sample_cov, tickers, big_tech_indices)
 
-    # Efficient Frontier plot (example) 
+    # Efficient Frontier plot (example)
     n_samples = 10000
-    w = np.random.dirichlet(np.ones(len(mu)), n_samples) 
-    rets = w.dot(mu) 
-    stds = np.sqrt(np.diag(w @ sample_cov @ w.T)) 
-    sharpes = rets / stds 
-    fig, ax = plt.subplots() 
-    plot_efficient_frontier_with_random_portfolios(ef, ax=ax, n_samples=n_samples)
-    ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r") 
-    ax.set_title("Efficient Frontier with random portfolios") 
-    ax.legend() 
+    w = np.random.dirichlet(np.ones(len(mu)), n_samples)
+    rets = w.dot(mu)
+    stds = np.sqrt(np.diag(w @ sample_cov @ w.T))
+    sharpes = rets / stds
+    fig, ax = plt.subplots()
+    plot_efficient_frontier_with_random_portfolios(
+        ef, ax=ax, n_samples=n_samples)
+    ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r")
+    ax.set_title("Efficient Frontier with random portfolios")
+    ax.legend()
     plt.tight_layout()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
